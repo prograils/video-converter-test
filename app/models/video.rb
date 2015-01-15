6201112bc99bfe68893ef62ab0f623339d671abb
Jarek Jeleniewicz <jarojele@gmail.com>
2015-01-15 15:41:46 +0100

    fix for using default video_file.url

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,9 @@
 class Video < ActiveRecord::Base
   ## UPLOADERS
   mount_uploader :video_file, VideoUploader do
-    def url(format)
+    def url(format = nil)
       uploaded_path = encode_path(file.path.sub(File.expand_path(root), ''))
+      return uploaded_path if format.nil?
       files = Dir.entries(File.dirname(file.path))
       files.each do |f|
         next unless File.extname(f) == '.' + format.to_s
